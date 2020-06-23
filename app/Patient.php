a10afc5c4cdf2924723a2ebd970c627068089e03
Muhammad Essam <mohammedessam699@gmail.com>
2020-06-24 01:37:38 +0200

    adds front numbers

--- app/Patient.php
+++ app/Patient.php
@@ -18,4 +18,19 @@ class Patient extends Model
     {
         return $this->belongsTo(Village::class, 'village_id', 'id');
     }
+
+    public static function Active()
+    {
+        return Patient::where('status', 'active');
+    }
+
+    public static function Cured()
+    {
+        return Patient::where('status', 'cured');
+    }
+
+    public static function Bands()
+    {
+        return Patient::where('band', true);
+    }
 }
