885cc1a3fa82d2a4e9fe1c849268e23242350a79
Qaisar Khursheed <qaisarkhursheed@hotmail.com>
2020-06-24 03:05:09 -0700

    adds to dashboard

--- resources/views/admin/layout/head.blade.php
+++ resources/views/admin/layout/head.blade.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Admin Panel</title>
+    <title>Covid19</title>
     <meta name="csrf-token" content="{{csrf_token()}}">
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,92 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @include('admin.layout.head')
 
-    <title>Laravel</title>
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@500&display=swap" rel="stylesheet">
-    <!-- Styles -->
-    <style>
-        html, body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Rajdhani', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links > a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 13px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-
-        .container {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            width: 70%;
-        }
-
-        .circle {
-            border: solid 5px #636b6f;
-            border-radius: 50%;
-            width: 200px;
-            height: 200px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-            margin: 5px;
-        }
-
-        h6 {
-            margin: 0px;
-            font-size: 35px;
-        }
-    </style>
-</head>
 <body>
 <div class="flex-center position-ref full-height">
     @if (Route::has('login'))
@@ -99,24 +15,100 @@
         </div>
     @endif
 
-    <div class="container">
-        <div class="circle">
-            <h6>{{\App\Patient::all()->count()}}</h6>
-            <h6>Total</h6>
+     <section class="content">
+         
+      <div class="container-fluid">
+          <center>
+            <img src="{{asset('admin/dist/img/moh.png')}}"  alt="User Image">
+       
+        
+        <br>       
+       
+<h1> Sinaw Hospital </h1>
+<h3>
+ لجنة التقصي الوبائي   
+
+ <br> Covid-19   
+</h3>
+<br>
+ </center>
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>{{\App\Patient::all()->count()}}</h3>
+
+                <p>TOTAL</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <a href="#" class="small-box-footer">المجموع</a>
+            </div>
+          </div>
+
+    <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>{{\App\Patient::Active()->count()}}</h3>
+
+                <p>ACTIVE</p></p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <a href="#" class="small-box-footer">نشط</a>
+            </div>
+          </div>
+          
+              <div class="col-lg-3 col-6">
+            <!-- small box -->
+<div class="small-box bg-gradient-success">
+              <div class="inner">
+                <h3>{{\App\Patient::Cured()->count()}}</h3>
+
+                <p>CURED</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <a href="#" class="small-box-footer">متعافى</a>
+            </div>
+          </div>
+          
+          
+              <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>{{\App\Patient::bands()->count()}}</h3>
+
+                <p>BAND</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <a href="#" class="small-box-footer">إسوارة</a></a>
+            </div>
+          </div>
+          
+          
+          
+     
+      
+          
+          <!-- ./col -->
         </div>
-        <div class="circle ">
-            <h6>{{\App\Patient::Active()->count()}}</h6>
-            <h6>Active</h6>
-        </div>
-        <div class="circle">
-            <h6>{{\App\Patient::Cured()->count()}}</h6>
-            <h6>Cured</h6>
-        </div>
-        <div class="circle ">
-            <h6>{{\App\Patient::Bands()->count()}}</h6>
-            <h6>Banded</h6>
-        </div>
-    </div>
+        <!-- /.row -->
+        <!-- Main row -->
+
+        <!-- /.row (main row) -->
+      </div><!-- /.container-fluid -->
+    </section>
+
 </div>
 </body>
 </html>
